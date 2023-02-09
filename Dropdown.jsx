6c34a27e84534fecd9f237e64934638cccc4a27d
Zedit42 <87221822+Zedit42@users.noreply.github.com>
2023-02-09 13:20:06 +0300

    Create Dropdown.jsx

---
+++ Dropdown.jsx
@@ -0,0 +1,7 @@
+                            <div class="hidden absolute text-center mt-[0.3rem] text-sm peer-hover:flex hover:flex
+                            w-[150px]
+                            flex-col bg-white drop-shadow-lg">
+                                <a class="px-5 py-3 hover:bg-gray-200 hover:text-black" href="#">About Us</a>
+                                <a class="px-5 py-3 hover:bg-gray-200" href="#">Contact Us</a>
+                                <a class="px-5 py-3 hover:bg-gray-200" href="#">Privacy Policy</a>
+                            </div>
